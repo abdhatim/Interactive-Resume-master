5ecc3211f8782c99668aafb23bdda66e887153bb
Abdelrahman Sakr <abdulrahmanhatem2@gmail.com>
2024-09-18 16:00:56 +0200

    ddd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main/Main";
-import Resume from "./pages/Resume/Resume";
+
 
 function App() {
   return (
