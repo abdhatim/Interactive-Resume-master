40d608d4631ebfc03a4e92bdc4c6679609a1fa7b
Abdelrahman Sakr <abdulrahmanhatem2@gmail.com>
2024-09-18 16:15:30 +0200

    ffff

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 const Resume = () => {
-  return (
-    <div className="resume-page">
-      <object
-        data="/resume.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      >
-        <p>
-          <a href="/resume.pdf">Download the CV</a>
-        </p>
-      </object>
-    </div>
-  );
+	
+	return (
+		<div className="resume-page">
+			<object
+				data="/resume.pdf"
+				type="application/pdf"
+				width="100%"
+				height="100%"
+			>
+				<p>
+					<a href="/resume.pdf">Download the CV</a>
+				</p>
+			</object>
+		</div>
+	);
 };
 
 export default Resume;
